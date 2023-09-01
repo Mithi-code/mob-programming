e16b8e51fd7ee70ab2d634dce10e763ad7b38ad1
Mithlesh kumar <mithlesh.9m@gmail.com>
2023-09-02 05:13:37 +0530

    Update salesByMatch.js

--- HackerRank sol/salesByMatch.js
+++ HackerRank sol/salesByMatch.js
@@ -1,20 +1,11 @@
 function sockMerchant(n, ar) {
-//Write code here
-let pair=0
-// sort the array elements
-ar.sort()
-// iterate through the array and compare the elements
-for(let i=0;i<n;i++){
-  if(ar[i]==ar[i+1]){
-// increment i iteration and add a pair
-    i++
-    pair++
-  }
+    // Write your code here
+    const uniqueValues = [... new Set(ar)]
+    let count = 0
+    uniqueValues.forEach(value => {
+        const filterValues = ar.filter(element => element == value)
+        const pairsNumber = Math.floor(filterValues.length/2)
+        count += pairsNumber
+    })
+    return count
 }
-return pair
-}
-
-// number of elements in the array
-let n=9
-// elements in the array
-let pairs=[10, 20, 20, 10, 10, 30, 50, 10, 20]
